4059ce644ec737a802bdcb2fe0de254cd53404ef
unknown <bappy.raj.bd@gmail.com>
2023-06-08 20:08:57 +0600

    generate token

--- controllers/generateToken.js
+++ controllers/generateToken.js
@@ -1,5 +1,22 @@
 const jwt = require('jsonwebtoken');
 
+exports.generateToken = async (req, res) => {
+  try {
+        const {userid} = req.body;
+        //console.log(userid);
+
+        const token = jwt.sign({ userid: userid }, process.env.JWT_SECRET, {
+        expiresIn: "7d",});
+        res.json({ token });
+        }catch (err) {
+          console.log(err);
+      }
+
+};
+
+
+
+/*
 function generateToken(userId, secretKey) {
   const payload = { userId };
   const options = {
@@ -8,11 +25,13 @@ function generateToken(userId, secretKey) {
 
   const token = jwt.sign(payload, secretKey, options);
   return token;
-}
+}*/
+
 
 // Example usage
-const userId = '123456789';
-const secretKey = 'your-secret-key';
+//const userId = '123456789';
+//const secretKey = 'your-secret-key';
 
 //const token = generateToken(userId, secretKey);
 //console.log(token);
+
--- routes/products.js
+++ routes/products.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const productController = require('../controllers/product');
 
 //token generate
-//const generateToken = require('../controllers/generateToken');
-//const authenticate = require('../middlewares/authenticate');
+const generateToken = require('../controllers/generateToken');
+const authenticate = require('../middlewares/authenticate');
 
 
-//router.get('/token', generateToken.generateToken);
-//router.post('/protected', authenticate);
+router.post('/token', generateToken.generateToken);
+router.post('/protected', authenticate);
 
-//router.post('/products', productController.createProduct);
+router.post('/products', productController.createProduct);
 
 router.get('/products',  productController.getAllProducts);
 
